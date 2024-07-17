c09b770bfecf97c75dbe7b6ae95ce72905745200
fastest4u <bangzerogg@gmail.com>
2024-07-17 20:33:45 +0700

    55

--- script.js
+++ script.js
@@ -1,18 +1,58 @@
+function logOut() {
+    liff.logout();
+    window.location.reload();
+  }
+  function logIn() {
+    liff.login({ redirectUri: window.location.href });
+  }
+  async function main() {
+    await liff.init({ liffId: "2005784883-MqXYJ2vW" });
+    if (liff.isLoggedIn()) {
+      const profile = await liff.getProfile();
+      const profileData = {
+        userId: profile.userId,
+        pictureUrl: profile.pictureUrl,
+      };
+      const userId = await checkLogin(profile.userId);
+      console.log(userId);
+      if (userId) {
+        sessionStorage.setItem("userData", JSON.stringify(userId));
+        $("#form-login").style.display("none");
+      } else {
+        console.log("ยังไม่ได้ลงทะเบียน");
+        // Optionally, you can call registerUser(profileData) here if you want to register new users automatically.
+      }
+    } else {
+      console.log("Not logged in");
+    }
+  }
 
-function checkLogin(userId) {
-    Url = "https://sheet.best/api/sheets/b1628384-d498-4a3e-819f-fb58c139df2a/tabs/users/userId/"
-    fetch(Url + userId)
-  .then((response) => response.json())
-  .then((data) => {
-    return data[0]
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-            
-        
-}
+  async function registerUser(userData) {
+    const apiUrl =
+      "https://sheet.best/api/sheets/b1628384-d498-4a3e-819f-fb58c139df2a/tabs/users";
+    const requestOptions = {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    };
 
-userId = "Ueba4de51638f6d4e11c70448cf1762d1"
+    try {
+      const response = await fetch(apiUrl, requestOptions);
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-checkLogin(userId)
+  async function checkLogin(userId) {
+    const Url = `https://sheet.best/api/sheets/b1628384-d498-4a3e-819f-fb58c139df2a/tabs/users/userId/${userId}`;
+    try {
+      const response = await fetch(Url);
+      const data = await response.json();
+      return data[0];
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  main();
